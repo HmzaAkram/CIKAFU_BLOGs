d71ce0595d171b5980130f60c1736f2c5b3e7833
Itxhamza7 <hmzaakram295@gmail.com>
2025-04-20 15:32:27 +0500

    working

--- cikafu/routes/api.php
+++ cikafu/routes/api.php
@@ -1,5 +1,8 @@
 <?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/ping', function () {
     return response()->json(['msg' => 'pong']);
 });
-Route::get('/posts', [PostController::class, 'index']);
