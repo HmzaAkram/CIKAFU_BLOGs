75eee93f642ea3cf942cb95472ced7e590495e0b
Itxhamza7 <hmzaakram295@gmail.com>
2025-04-21 11:41:21 +0500

    Update web.php

--- cikafu/routes/web.php
+++ cikafu/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/db-check', function () {
+    try {
+        \DB::connection()->getPdo();
+        return '✅ DB Connected!';
+    } catch (\Exception $e) {
+        return '❌ DB Error: ' . $e->getMessage();
+    }
+});
